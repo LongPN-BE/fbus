78ea836addf007f573fdfa03bb40185ffcb9d384
Pham Ngoc Long <94893379+LongPN-BE@users.noreply.github.com>
2022-09-25 20:31:16 +0700

    first commit

---
+++ src/main/java/com/example/fbus/FbusApplication.java
@@ -0,0 +1,13 @@
+package com.example.fbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fbus/FbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
